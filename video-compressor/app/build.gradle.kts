86d3acc67dc8fb56ddfc7733d78ffaf980777acf
joyceyeh306 <joyceyeh@msn.com>
2026-09-12 18:12:00 +0800

    Bump Video Compressor to v1.9

--- video-compressor/app/build.gradle.kts
+++ video-compressor/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.joyce.videocompressor"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.8"
+        versionCode = 10
+        versionName = "1.9"
     }
 
     compileOptions {
