cab8d470461191cc7ee91cac180168e2cd0ce97a
joyceyeh306 <joyceyeh@msn.com>
2026-09-13 19:10:20 +0800

    Add Ajo IME service

---
+++ imeapp/app/src/main/java/tw/ajo/ime/AjoImeService.java
@@ -0,0 +1,54 @@
+package tw.ajo.ime;
+
+import android.inputmethodservice.InputMethodService;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+import android.widget.Toast;
+
+public class AjoImeService extends InputMethodService {
+    private IosKeyboardView keyboard;
+
+    @Override public View onCreateInputView() {
+        keyboard = new IosKeyboardView(this, this);
+        return keyboard;
+    }
+
+    @Override public boolean onEvaluateFullscreenMode() { return false; }
+
+    @Override public void onStartInput(EditorInfo attribute, boolean restarting) {
+        super.onStartInput(attribute, restarting);
+        if (keyboard != null) keyboard.onEditorChanged(attribute);
+    }
+
+    public void commit(String s) {
+        InputConnection ic = getCurrentInputConnection();
+        if (ic != null) ic.commitText(s, 1);
+    }
+
+    public void backspace() {
+        InputConnection ic = getCurrentInputConnection();
+        if (ic == null) return;
+        CharSequence before = ic.getTextBeforeCursor(1, 0);
+        if (before != null && before.length() > 0) ic.deleteSurroundingText(1, 0);
+        else ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DEL));
+    }
+
+    public void enter() {
+        InputConnection ic = getCurrentInputConnection();
+        EditorInfo info = getCurrentInputEditorInfo();
+        if (ic == null) return;
+        int action = info == null ? EditorInfo.IME_ACTION_NONE : (info.imeOptions & EditorInfo.IME_MASK_ACTION);
+        if (action != EditorInfo.IME_ACTION_NONE && action != EditorInfo.IME_ACTION_UNSPECIFIED) {
+            ic.performEditorAction(action);
+        } else {
+            ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
+            ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_ENTER));
+        }
+    }
+
+    public void voiceComingSoon() {
+        Toast.makeText(this, "語音輸入會在第二階段加入", Toast.LENGTH_SHORT).show();
+    }
+}
