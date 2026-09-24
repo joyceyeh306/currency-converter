46ce6ee6009ccb383892b00559349e40ee05a479
joyceyeh306 <joyceyeh@msn.com>
2026-09-24 22:51:40 +0800

    Refresh iPhone home screen icon

--- viewer/sw.js
+++ viewer/sw.js
@@ -1,3 +1,3 @@
-const CACHE='ajo-where-v1';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./index.html','./manifest.webmanifest','./apple-touch-icon.png']))));
+const CACHE='ajo-where-v2';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./index.html','./manifest.webmanifest','./apple-touch-icon-v2.png']))));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
