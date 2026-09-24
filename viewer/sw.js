3319e77ab758e34a31ffe208f59cdb5d9551464b
joyceyeh306 <joyceyeh@msn.com>
2026-09-24 22:58:40 +0800

    Fix iPhone home screen icon loading

--- viewer/sw.js
+++ viewer/sw.js
@@ -1,3 +1,3 @@
-const CACHE='ajo-where-v2';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./index.html','./manifest.webmanifest','./apple-touch-icon-v2.png']))));
+const CACHE='ajo-where-v3';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./index.html','./manifest.webmanifest','./apple-touch-icon.png?v=20260924c?v=20260924c']))));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
