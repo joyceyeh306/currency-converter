4f68748c90163ea03c9b3ed021346384f221bd0a
joyceyeh306 <joyceyeh@msn.com>
2026-09-26 19:15:18 +0800

    Fix pointer event consume import

--- photo_manager_app/app/src/main/java/tw/ajo/photomanager/ViewerScreen.kt
+++ photo_manager_app/app/src/main/java/tw/ajo/photomanager/ViewerScreen.kt
@@ -81,7 +81,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
-import androidx.compose.ui.input.pointer.consume
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.onSizeChanged
