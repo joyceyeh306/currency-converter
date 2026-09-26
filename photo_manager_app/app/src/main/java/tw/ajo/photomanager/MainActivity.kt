75b040b2f60b6ae1972fa2651592a79697ce8e72
joyceyeh306 <joyceyeh@msn.com>
2026-09-26 17:49:13 +0800

    Repair Kotlin file header and Compose import

--- photo_manager_app/app/src/main/java/tw/ajo/photomanager/MainActivity.kt
+++ photo_manager_app/app/src/main/java/tw/ajo/photomanager/MainActivity.kt
@@ -7,6 +7,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.Window
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
